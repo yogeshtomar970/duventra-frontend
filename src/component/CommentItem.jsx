a7461d24f808d2452643da90460038f85a37465a
yogesh tomar <yogeshtomar970@gmail.com>
2026-05-22 13:51:51 +0530

    commentitem.jsx update

--- src/component/CommentItem.jsx
+++ src/component/CommentItem.jsx
@@ -6,7 +6,20 @@ export default function CommentItem({ item }) {
   return (
     <div className="comment-item">
       <div className="comment-avatar">
-        {(item.userName || item.userId || "U")[0].toUpperCase()}
+        {item.profilePic ? (
+          <img
+            src={item.profilePic}
+            alt={item.userName}
+            style={{
+              width: "100%",
+              height: "100%",
+              borderRadius: "50%",
+              objectFit: "cover",
+            }}
+          />
+        ) : (
+          (item.userName || item.userId || "U")[0].toUpperCase()
+        )}
       </div>
 
       <div className="comment-body">

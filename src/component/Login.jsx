63118763384730d9462b83af45205b9e607bf98b
yogesh tomar <yogeshtomar970@gmail.com>
2026-07-02 09:53:33 +0530

    login.jsx update

--- src/component/Login.jsx
+++ src/component/Login.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { showSuccess, showError } from "../utils/toast.js";
+import { showSuccess, showError } from "../utils/Toast.js";
 import API_BASE_URL from "../config/api.js";
 
 /**
